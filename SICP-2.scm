e44627ce042b21a8c153be763b87501385b541f8
Linden Schrage <lschrage@college.harvard.edu>
2023-06-10 10:55:13 -0400

    ex 2.2

--- SICP-2.scm
+++ SICP-2.scm
@@ -1,5 +1,4 @@
 ; Representing rational numbers
-
 (define (error str)
     (display str))
 
@@ -8,7 +7,7 @@
       a
       (gcd b (remainder a b))))
 
-(define (make-rat n d)
+(define (make-rat-old n d)
     (let ((g (gcd n d)))
         (cons (/ n g) (/ d g))))
 
@@ -58,11 +57,42 @@
 (define n4 (make-rat-improved 1 2))
 
 ; Exercise 2.1
-
-(define (make-rat-improved n d)
+(define (make-rat n d)
     (define g (gcd n d))
         (cond
            ( (= d 0) (error "The denominator can't be zero!"))
            ( (> (* n d) 0) (cons (abs (/ n g)) (abs (/ d g)) ) )
            ( (< (* n d) 0) (cons (* -1 (abs (/ n g))) (abs (/ d g)) ) ) ) ) 
+
+; Exercise 2.2
+(define (point x-point y-point)
+    (cons x-point y-point))
+
+(define (x-point p)
+    (car p))
+
+(define (y-point p)
+    (cdr p))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment start-segment end-segment)
+    (cons start-segment end-segment))
+
+(define (midpoint-segment segment)
+    (point (average (x-point (car segment)) (x-point (cdr segment)))
+           (average (y-point (car segment)) (y-point (cdr segment)))))
+
+(define p1 (point 1 2))
+(define p2 (point 1 8))
+(define seg (make-segment p1 p2))
+(define mid (midpoint-segment seg))
+
+
 
